fc52357ee63a0ce2993f86b8a107b3932dbb7743
changbin1997 <zsmcbdyx@foxmail.com>
2019-08-04 17:29:13 +0800

    优化了可访问性和代码可读性

--- page-archive.php
+++ page-archive.php
@@ -8,44 +8,48 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('header.php');
 ?>
 
-<div class="container archive-page">
-    <div class="row">
-        <div class="archive col-md-12 col-lg-8 col-sm-12">
-            <main>
-                <header class="entry-header">
-                    <h2 class="entry-title p-name">
-                        <a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-                    </h2>
-                </header>
-                <article>
-                    <div class="post-content" itemprop="articleBody">
-                        <?php
-                        $stat = Typecho_Widget::widget('Widget_Stat');
-                        Typecho_Widget::widget('Widget_Contents_Post_Recent', 'pageSize='.$stat->publishedPostsNum)->to($archives);
-                        $year=0; $mon=0; $i=0; $j=0;
-                        $output = '<div class="archives">';
-                        while($archives->next()){
-                            $year_tmp = date('Y',$archives->created);
-                            $mon_tmp = date('m',$archives->created);
-                            $y=$year; $m=$mon;
-                            if ($year > $year_tmp || $mon > $mon_tmp) {
-                                $output .= '</ul></div>';
+    <div class="container archive-page">
+        <div class="row">
+            <div class="archive col-md-12 col-lg-8 col-sm-12">
+                <main>
+                    <header class="entry-header">
+                        <h2 class="entry-title p-name">
+                            <a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                        </h2>
+                    </header>
+                    <article>
+                        <div class="post-content" itemprop="articleBody">
+                            <?php
+                            $stat = Typecho_Widget::widget('Widget_Stat');
+                            Typecho_Widget::widget('Widget_Contents_Post_Recent', 'pageSize=' . $stat->publishedPostsNum)->to($archives);
+                            $year = 0;
+                            $mon = 0;
+                            $i = 0;
+                            $j = 0;
+                            $output = '<div class="archives">';
+                            while ($archives->next()) {
+                                $year_tmp = date('Y', $archives->created);
+                                $mon_tmp = date('m', $archives->created);
+                                $y = $year;
+                                $m = $mon;
+                                if ($year > $year_tmp || $mon > $mon_tmp) {
+                                    $output .= '</ul></div>';
+                                }
+                                if ($year != $year_tmp || $mon != $mon_tmp) {
+                                    $year = $year_tmp;
+                                    $mon = $mon_tmp;
+                                    $output .= '<div class="archives-item"><h4>' . date('Y年m月', $archives->created) . '</h4><hr><ul class="archives_list" aria-label="' . date('Y年m月', $archives->created) . '">'; //输出年份
+                                }
+                                $output .= '<li>' . date('d日', $archives->created) . ' <a href="' . $archives->permalink . '">' . $archives->title . '</a></li>'; //输出文章
                             }
-                            if ($year != $year_tmp || $mon != $mon_tmp) {
-                                $year = $year_tmp;
-                                $mon = $mon_tmp;
-                                $output .= '<div class="archives-item"><h4>'.date('Y年m月',$archives->created).'</h4><hr><ul class="archives_list">'; //输出年份
-                            }
-                            $output .= '<li>'.date('d日',$archives->created).' <a href="'.$archives->permalink .'">'. $archives->title .'</a></li>'; //输出文章
-                        }
-                        $output .= '</ul></div></div>';
-                        echo $output;
-                        ?>
-                    </div>
-                </article>
-            </main>
+                            $output .= '</ul></div></div>';
+                            echo $output;
+                            ?>
+                        </div>
+                    </article>
+                </main>
+            </div>
+            <?php $this->need('sidebar.php'); ?>
         </div>
-        <?php $this->need('sidebar.php'); ?>
     </div>
-</div>
 <?php $this->need('footer.php'); ?>
