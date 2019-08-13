b3a3ac2c733371955de6f42b4468c88dc0a51f75
changbin1997 <zsmcbdyx@foxmail.com>
2019-08-13 15:52:32 +0800

    全站友情链接从底部显示改为侧边栏显示

--- footer.php
+++ footer.php
@@ -1,15 +1,5 @@
 <footer>
     <div class="container">
-        <?php if ($this->options->homeLinks): ?>
-            <nav aria-label="友情链接" role="navigation">
-                <h5>友情链接</h5>
-                <?php $links = json_decode($this->options->homeLinks); ?>
-                <?php for ($i = 0; $i < count($links); $i++): ?>
-                    <a target="_blank" href="<?php echo $links[$i]->url; ?>"
-                       title="<?php echo $links[$i]->title; ?>"><?php echo $links[$i]->name; ?></a>
-                <?php endfor; ?>
-            </nav>
-        <?php endif; ?>
         <span>Powered by <a href="http://www.typecho.org/" target="_blank">Typecho</a> Theme by <a href="https://www.misterma.com/archives/812/" target="_blank">MWordStar</a></span>
     </div>
 </footer>
