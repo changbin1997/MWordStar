6b72cd1749b7751e34379db403f0543091930013
changbin1997 <zsmcbdyx@foxmail.com>
2019-08-13 15:53:40 +0800

    增加了首页友链

--- page-links.php
+++ page-links.php
@@ -20,8 +20,34 @@ $this->need('header.php');  //  头文件
                 <article>
                     <div class="post-content">
                         <!--内页链接区域-->
+                        <?php if ($this->options->pageLinks): ?>
+                            <h3>内页链接</h3>
+                            <?php $linkList = json_decode($this->options->pageLinks); ?>
+                            <div class="row link-box">
+                                <?php for ($i = 0;$i < count($linkList);$i ++): ?>
+                                    <div class="col-lg-6 col-md-6 col-sm-12 col-12 link-grid">
+                                        <div class="link">
+                                            <a href="<?php echo $linkList[$i]->url; ?>" class="clearfix" target="_blank">
+                                                <?php if (!isset($linkList[$i]->logoUrl) or $linkList[$i]->logoUrl == ''): ?>
+                                                    <i class="link-logo float-left icon-link icon-logo" aria-label="站点Logo"></i>
+                                                <?php else: ?>
+                                                    <img src="<?php echo $linkList[$i]->logoUrl; ?>" alt="站点Logo" class="link-logo float-left">
+                                                <?php endif; ?>
+                                                <span class="link-name float-left"><?php echo $linkList[$i]->name; ?></span>
+                                            </a>
+                                            <?php if (!isset($linkList[$i]->title) or $linkList[$i]->title == ''): ?>
+                                                <p>暂无简介</p>
+                                            <?php else: ?>
+                                                <p><?php echo $linkList[$i]->title; ?></p>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                <?php endfor; ?>
+                            </div>
+                        <?php endif; ?>
+                        <!--全站链接区域-->
                         <?php if ($this->options->links): ?>
-                        <h3>内页链接</h3>
+                        <h3>全站链接</h3>
                         <?php $linkList = json_decode($this->options->links); ?>
                         <div class="row link-box">
                             <?php for ($i = 0;$i < count($linkList);$i ++): ?>
@@ -45,9 +71,9 @@ $this->need('header.php');  //  头文件
                             <?php endfor; ?>
                         </div>
                         <?php endif; ?>
-                        <!--全站链接区域-->
+                        <!--首页链接区域-->
                         <?php if ($this->options->homeLinks): ?>
-                        <h3>全站链接</h3>
+                        <h3>首页链接</h3>
                         <?php $links = json_decode($this->options->homeLinks); ?>
                         <div class="row link-box">
                             <?php for ($i = 0;$i < count($links);$i ++): ?>
