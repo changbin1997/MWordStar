309f1af8ec288234ef353eaaccc19b85547ba46d
changbin1997 <zsmcbdyx@foxmail.com>
2020-06-14 23:55:48 +0800

    版权信息区域的主题链接改为指向主题的 Github 仓库

--- components/footer.php
+++ components/footer.php
@@ -6,7 +6,7 @@
                 <span><?php $this->options->icp(); ?></span>
             </div>
         <?php endif; ?>
-        <span>Powered by <a href="http://www.typecho.org/" target="_blank">Typecho</a> Theme by <a href="https://www.misterma.com/archives/812/" target="_blank">MWordStar</a></span>
+        <span>Powered by <a href="http://www.typecho.org/" target="_blank">Typecho</a> Theme by <a href="https://github.com/changbin1997/MWordStar" target="_blank">MWordStar</a></span>
     </div>
 </footer>
 <?php if ($this->options->toTop == 'show'): ?>
