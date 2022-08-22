0cb36ad02cd5ad82e346433ee6d9b89e1ad77e76
changbin1997 <zsmcbdyx@foxmail.com>
2022-08-22 23:58:30 +0800

    修复了删除 Bootstrap 组件导致图片灯箱按钮样式丢失的问题

--- components/max-img.php
+++ components/max-img.php
@@ -3,19 +3,19 @@
     <img src="" alt="" class="shadow-lg">
     <p id="img-info" class="text-light text-center"></p>
     <div class="btn-group" role="group" aria-label="图片工具栏" id="img-control">
-        <button type="button" class="btn btn-dark big" title="放大" aria-label="放大">
+        <button type="button" class="btn big" title="放大" aria-label="放大">
             <i class="icon-zoom-in"></i>
         </button>
-        <button type="button" class="btn btn-dark small" title="缩小" aria-label="缩小">
+        <button type="button" class="btn small" title="缩小" aria-label="缩小">
             <i class="icon-zoom-out"></i>
         </button>
-        <button type="button" class="btn btn-dark spin-left" title="左旋转90度" aria-label="左旋转90度">
+        <button type="button" class="btn spin-left" title="左旋转90度" aria-label="左旋转90度">
             <i class="icon-undo"></i>
         </button>
-        <button type="button" class="btn btn-dark spin-right" title="右旋转90度" aria-label="右旋转90度">
+        <button type="button" class="btn spin-right" title="右旋转90度" aria-label="右旋转90度">
             <i class="icon-redo"></i>
         </button>
-        <button type="button" class="btn btn-dark hide-img" title="关闭大图（ESC）" aria-label="关闭大图（ESC）">
+        <button type="button" class="btn hide-img" title="关闭大图（ESC）" aria-label="关闭大图（ESC）">
             <i class="icon-cancel-circle"></i>
         </button>
     </div>
