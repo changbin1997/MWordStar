4f4b2abab5af0503755650e6de6c45f91912ce6e
changbin1997 <zsmcbdyx@foxmail.com>
2026-09-09 18:33:52 +0800

    加入了并排显示布局的短代码，修复了短代码解析后会出现 p 套 div 的情况。

--- inc/content.php
+++ inc/content.php
@@ -224,7 +224,7 @@ function parseThemeShortcodes($content, $cid = 0) {
     static $collapse_id = 0;
     static $tabs_id = 0;
     // 定义支持的短代码标签，方便未来维护和添加新功能
-    $supported_tags = array('button', 'alert', 'collapse', 'badge', 'hide', 'progress', 'tabs');
+    $supported_tags = array('button', 'alert', 'collapse', 'badge', 'hide', 'progress', 'tabs', 'row', 'col');
     $tags_pattern = implode('|', $supported_tags);
 
     // 渲染单个短代码为 HTML；$rawText 为短代码原始文本，用于无法解析时兜底原样返回
@@ -299,7 +299,7 @@ function parseThemeShortcodes($content, $cid = 0) {
                 }
                 // 有权限时返回隐藏内容本体，无权限时返回提示信息
                 return canViewHideContent($type, $cid)
-                    ? preg_replace('/^\<br>|\<br>$/', '', $inner_content)
+                    ? preg_replace('/^\<br>/', '', $inner_content)
                     : '<div class="alert expiration-reminder">' . $tip . '</div>';
 
             case 'progress':
@@ -353,11 +353,20 @@ function parseThemeShortcodes($content, $cid = 0) {
 
                     return '<div class="tab-box">'
                         . '<ul class="nav nav-tabs" role="tablist">' . $nav_html . '</ul>'
-                        . '<div class="tab-content border-left border-right border-bottom" id="' . $widgetId . '-content">' . $pane_html . '</div>'
+                        . '<div class="tab-content border" id="' . $widgetId . '-content">' . $pane_html . '</div>'
                         . '</div>';
                 }
                 // 没有解析到任何 tab 时返回原文本
                 return $rawText;
+
+            case 'row':
+                // 行容器：包一层 tc-row，内部列由 col 短代码渲染
+                $re = '/<div\s+class="tc-col">.*?<\/div>(*SKIP)(*FAIL)|<br\s*\/?>/s';
+                return '<div class="tc-row">' . preg_replace($re, '', $inner_content) . '</div>';
+
+            case 'col':
+                // 列：包一层 tc-col；与 collapse / tabs 一致，去掉首尾多余的 <br>
+                return '<div class="tc-col">' . preg_replace('/^\<br>|\<br>$/', '', $inner_content) . '</div>';
 
             default:
                 // 如果没有对应的处理逻辑，返回原文本
@@ -449,7 +458,26 @@ function parseThemeShortcodes($content, $cid = 0) {
         return $result;
     };
 
-    return $renderContent($content);
+    $html = $renderContent($content);
+
+    // Typecho 的 Markdown 会把短代码当成普通段落，给解析出的块级 div 外层套上 <p>；
+    // 块级 div 不能嵌套在 <p> 内，这里去掉仅包含 div 块的段落外层 <p>（普通文本段落不受影响）
+    $unwrapDivParagraph = function ($text) {
+        // 匹配 <p> 内仅包含一个或多个（可嵌套）<div> 块的段落
+        $pattern = '#<p>\s*((?:<div\b[^>]*>(?:(?1)|(?!</?div\b).)*</div>)\s*)+</p>#is';
+        while (true) {
+            $new = preg_replace_callback($pattern, function ($matches) {
+                return $matches[1];
+            }, $text);
+            if ($new === null || $new === $text) {
+                break;
+            }
+            $text = $new;
+        }
+        return $text;
+    };
+
+    return $unwrapDivParagraph($html);
 }
 
 /**
@@ -517,7 +545,7 @@ function canViewHideContent($type, $cid = 0) {
  */
 function stripThemeShortcodes($content) {
     // 定义支持的短代码标签，与 parseThemeShortcodes 保持一致；tab 附属于 tabs，单独列出以便摘要去除
-    $supported_tags = array('button', 'alert', 'collapse', 'badge', 'hide', 'progress', 'tabs', 'tab');
+    $supported_tags = array('button', 'alert', 'collapse', 'badge', 'hide', 'progress', 'tabs', 'tab', 'row', 'col');
     $tags_pattern = implode('|', $supported_tags);
     // 前半部分匹配 <pre> / <code> 块（忽略其中的短代码）
     // 后半部分匹配 [tag ...]内容[/tag] 的短代码
